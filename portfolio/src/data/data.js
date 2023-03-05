4594e443a3036ccc2ed553bc7850ffafeadeeec7
Reason Shrestha <sthareason264@gmail.com>
2023-03-05 18:58:27 +0545

    Populated data.js file with base data

---
+++ portfolio/src/data/data.js
@@ -0,0 +1,19 @@
+export const data = {
+    skills: [
+        {id: 1, name: 'JavaScript', level: 7, baseImageUrl: ''},
+        {id: 1, name: 'JavaScript', level: 7, baseImageUrl: ''},
+        {id: 1, name: 'JavaScript', level: 7, baseImageUrl: ''},
+        {id: 1, name: 'JavaScript', level: 7, baseImageUrl: ''},
+        {id: 1, name: 'JavaScript', level: 7, baseImageUrl: ''},
+        {id: 1, name: 'JavaScript', level: 7, baseImageUrl: ''},
+    ],
+    projects: [
+        {id: 1, title: 'Doctor Appointment Booking System', baseImageUrl: '', githubUrl: ''},
+        {id: 1, title: 'Doctor Appointment Booking System', baseImageUrl: '', githubUrl: ''},
+        {id: 1, title: 'Doctor Appointment Booking System', baseImageUrl: '', githubUrl: ''},
+        {id: 1, title: 'Doctor Appointment Booking System', baseImageUrl: '', githubUrl: ''},
+        {id: 1, title: 'Doctor Appointment Booking System', baseImageUrl: '', githubUrl: ''},
+        {id: 1, title: 'Doctor Appointment Booking System', baseImageUrl: '', githubUrl: ''},
+        {id: 1, title: 'Doctor Appointment Booking System', baseImageUrl: '', githubUrl: ''},
+    ]
+}
